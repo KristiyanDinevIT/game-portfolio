f1f3d58172dda54833e4bedbbb2ebebc7716b93b
KristiyanDinevIT <et.kristiyan.d@eduiteh.eu>
2026-01-23 12:09:53 +0200

    Initial commit
    Signed-off-by: KristiyanDinevIT <et.kristiyan.d@eduiteh.eu>

---
+++ index.js
@@ -0,0 +1,69 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 64 * 16
+canvas.height = 64 * 9
+
+const collisionBlocks = []
+
+const parsedCollisions = collisionsLevel1.parse2D()
+parsedCollisions.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if(symbol === 292) {
+            collisionBlocks.push(
+                new collisionBlock({
+                position: {
+                    x: x * 64,
+                    y: y * 64,
+                },
+            })
+          )
+        }
+    })
+})
+
+const backgroundLevel1 = new Sprite ({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    ImageSrc: './img/backgroundLevel1.png'
+})
+
+const player = new Player({
+    collisionBlocks,
+})
+
+const keys = {
+    w: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+
+    backgroundLevel1.draw()
+    collisionBlocks.forEach(collisionBlock => {
+        collisionBlock.draw()
+    })
+
+    player.velocity.x = 0
+    if (keys.d.pressed){
+        player.velocity.x = 5
+    } else if (keys.a.pressed){
+        player.velocity.x = -5
+    } 
+
+    player.draw()
+    player.update()
+
+}
+
+animate()
---
+++ js/classes/Player.js
@@ -0,0 +1,59 @@
+class Player {
+    constructor({
+        collisionBlocks = []
+    }) {
+        this.position = {
+            x: 100,
+            y: 100, 
+        }
+
+        this.velocity = {
+            x: 0,
+            y: 0,
+        }
+
+        this.width = 100
+        this.height = 100
+        this.sides = {
+            bottom: this.position.y + this.height
+        }
+        this.gravity = 1
+
+        this.collisionBlocks = collisionBlocks
+        console.log(this.collisionBlocks)
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.position.x += this.velocity.x
+        for(let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i]
+            if(this.position.x <= collisionBlock.position.x + collisionBlock.width && 
+                this.position.x + this.width >= collisionBlock.position.x &&
+                this.position.y + this.height >= collisionBlock.position.y &&
+                this.position.y <= collisionBlock.position.y + collisionBlock.height
+            ) {
+                if(this.velocity. x < -1) {
+                    this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01
+                    break
+                }
+                if(this.velocity.x > 1) {
+                    this.position.x = collisionBlock.position.x - this.width - 0.01
+                    break
+                }
+            }
+        }
+        this.position.y += this.velocity.y
+        this.sides.bottom = this.position.y + this.height
+
+        // above bottom of canvas 
+        if (this.sides.bottom + this.velocity.y < canvas.height) {
+            this.velocity.y += this.gravity 
+        } else this.velocity.y = 0
+    }
+
+}
